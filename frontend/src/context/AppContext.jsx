ce64e0a137d222efb380079c1868a537b8e16250
sundram <ram322222@gmail.com>
2025-06-14 14:58:43 +0530

    created api to load doctor data

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -33,7 +33,24 @@ const AppContextProvider = (props) => {
     }
 
     // Getting User Profile using API
-   
+    const loadUserProfileData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+
+            if (data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
 
     useEffect(() => {
         getDoctosData()
